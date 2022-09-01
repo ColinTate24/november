817bc0fb9cc33498c8740c2557a43d25340a4ba5
ColinTate24 <97544852+ColinTate24@users.noreply.github.com>
2022-09-01 13:28:26 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express')
+const path = require('node:path')
+
 const app = express()
 
+let userName = "Joe";
+
 app.get('/', function (req, res) {
-  res.send('Hello World')
+
+  res.sendFile(path.join(__dirname, "index.html" ));
+
+  // res.send('Hello ' + userName + ' from Node/Express/Heroku')
+
+  res.send(`Hello ${userName} from Node/Express/Heroku with Backticks`)
 })
 
-app.listen(process.env.PORT || 3000, 
-	() => console.log("Server is running..."));
+app.listen(process.env.PORT || 3000,
+  () => console.log("Server is running..."));
