00d542d0fb50833d2f3b1697e392ba54e82d5269
ColinTate24 <97544852+ColinTate24@users.noreply.github.com>
2022-09-01 13:44:49 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ const path = require('node:path')
 
 const app = express()
 
-let userName = "Joe";
+app.set("view engine", "ejs")
+
+let userNameExpress = "Joel";
 
 app.get('/', function (req, res) {
 
-  res.sendFile(path.join(__dirname, "index.html" ));
-
+  // res.sendFile(path.join(__dirname, "index.html" ));
   // res.send('Hello ' + userName + ' from Node/Express/Heroku')
+  // res.send(`Hello ${userName} from Node/Express/Heroku with Backticks`)
 
-  res.send(`Hello ${userName} from Node/Express/Heroku with Backticks`)
+  res.render('index', { userNameEjs: userNameExpress})
 })
 
 app.listen(process.env.PORT || 3000,
