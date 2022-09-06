19213dbb92642c3a1c2422d01a7d9bf946401862
Tate <ctate2@una.edu>
2022-09-06 13:08:02 -0500

    fixing the thing

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
 const express = require('express')
 const path = require('node:path')
+const bodyParser = require('body-parser')
 
 const app = express()
 
-app.set("view engine", "ejs")
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'ejs');
 
-let userNameExpress = "Joel";
+
+
+
+
+
+let userName = "Joelz"; 
+let thatData = ""; 
 
 app.get('/', function (req, res) {
+    // res.sendFile(path.join(__dirname, "index.html" )); 
+    // res.send('Hello ' + userName + ' from Node/Express/Heroku');
+    // res.send(`Hello ${userName} from Node/Express/Heroku with Backticks!`)
 
-  // res.sendFile(path.join(__dirname, "index.html" ));
-  // res.send('Hello ' + userName + ' from Node/Express/Heroku')
-  // res.send(`Hello ${userName} from Node/Express/Heroku with Backticks`)
+    res.render('index', 
+      { 
+        userName: "userName"
+      }
+      );
 
-  res.render('index', { userNameEjs: userNameExpress})
+})
+
+
+app.post("/saveToNode", (req, res) => {
+  console.log(req.body);
+  console.group(req.body.userName);
+  res.render('index', { userName: req.body.userName });
+
 })
 
 app.listen(process.env.PORT || 3000,
